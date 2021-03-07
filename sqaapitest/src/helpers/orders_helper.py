0944ad207019d3febd3837381507ab837331de92
KiranPawar72 <kirpawar014@gmail.com>
2021-03-07 15:53:43 +0530

    Added orders update and retrive helper methods

--- sqaapitest/src/helpers/orders_helper.py
+++ sqaapitest/src/helpers/orders_helper.py
@@ -1,5 +1,6 @@
 
 from sqaapitest.src.utilities.wooAPIUtility import WooAPIUtility
+from sqaapitest.src.dao.orders_dao import OrdersDAO
 import os
 import json
 
@@ -23,3 +24,38 @@ class OrdersHelper(object):
         rs_api = self.woo_helper.post('orders', params=payload, expected_status_code=201)
 
         return rs_api
+
+    @staticmethod
+    def verify_order_is_created(order_json, exp_cust_id, exp_products):
+        orders_dao = OrdersDAO()
+
+        # verify response
+        assert order_json, f"Create order response is empty."
+        assert order_json['customer_id'] == exp_cust_id, f"Create order with given customer id returned" \
+                                                         f"bad customer id. Expected customer_id={exp_cust_id}" \
+                                                         f"but got '{order_json['customer_id']}"
+
+        assert len(order_json['line_items']) == len(exp_products), f"Expected only {exp_products} item in order but " \
+                                                   f"found '{len(order_json['line_items'])}'" \
+                                                   f"Order id: {order_json['id']}."
+
+        # verify db
+        order_id = order_json['id']
+        line_info = orders_dao.get_order_lines_by_order_id(order_id)
+        assert line_info, f"Create order, line item not found in DB. Order id: {order_id}"
+
+        line_items = [i for i in line_info if i['order_item_type'] == 'line_item']
+        assert len(line_items), f"Expected 1 line item but found {len(line_items)}. Order id: {order_id}"
+
+        # get list of product ids in the response
+        api_product_ids = [i['product_id'] for i in order_json['line_items']]
+
+        for product in exp_products:
+            assert product['product_id'] in api_product_ids, f"Create order does not have at least 1 expected product in DB." \
+                                                            f"Product id: {product['product_id']}. Order id: {order_id}"
+
+    def call_update_an_order(self, order_id, payload):
+        return self.woo_helper.put(f'orders/{order_id}', params=payload)
+
+    def call_retrieve_an_order(self, order_id):
+        return self.woo_helper.get(f"orders/{order_id}")
